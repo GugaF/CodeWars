a0b744e52fcbb4335cbb096952fd3d190fdb066e
GugaF <gugaf@networkgtc.com>
2023-01-10 23:38:08 +0200

    2023-01-10: 8th Kyu - Multiply

---
+++ 2023-01-10 codewars/app.js
@@ -0,0 +1,11 @@
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  return a * b; // missing return keyword
+}
+
+
+Console.log (multiply(2,1)), 2;
+Console.log (multiply(2,2)), 4;
+Console.log (multiply(3,5)), 15;   
+Console.log (multiply(1,1)), 1;
