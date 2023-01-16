4749c90124f5106022cd43fae4fb57f9cecb7976
GugaF <gugaf@networkgtc.com>
2023-01-16 14:37:56 +0200

    2023-01-16: 8th Kyu - Reversed Strings

---
+++ 2023-01-16 codewars/app.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+// 8th Kyu - Reversed Strings
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+
+
+function solution(str){
+    return str.split("").reverse().join("");
+  }
+
+console.log(solution('world'), 'dlrow');
+console.log(solution('hello'), 'olleh');
+console.log(solution(''), '');
+console.log(solution('h'), 'h');
