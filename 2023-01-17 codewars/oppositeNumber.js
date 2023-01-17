18c68d41edc94dda210a8e0ed400d41719b1de62
GugaF <gugaf@networkgtc.com>
2023-01-17 20:46:14 +0200

    2023-01-17: 8th Kyu - Opposite Number

---
+++ 2023-01-17 codewars/oppositeNumber.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+// 8th Kyu - Opposite Nuumber
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+    return -number //=== 0 ? number : number * -1;
+}
+
+console.log(opposite(1), -1);
+console.log(opposite(0), 0);
+console.log(opposite(4.25), -4.25);
+console.log(opposite(3.3333333), -3.3333333);
+console.log(opposite(-12525220.3325), 12525220.3325);
+console.log(opposite(-5), 5);
