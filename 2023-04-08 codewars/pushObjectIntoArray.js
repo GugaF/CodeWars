f8b1537e06e9f443f6050205f4a2966308ff6bdf
GugaF <gugaf@networkgtc.com>
2023-04-08 21:30:34 +0200

    2023-04-08: 8th Kyu - Push Object into Array

---
+++ 2023-04-08 codewars/pushObjectIntoArray.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/527b3cd0492b6b15250060af/train/javascript
+// 8th Kyu - Push an object into array
+
+// You are trying to put an object in javascript into an array, 
+// but it always returns error, solve it and keep it as simple as possible!
+
+items = [];
+items.push({a: "b", c: "d"});
+// solution is to put the object inside brackets()
