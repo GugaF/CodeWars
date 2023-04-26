a40deeba2ee535206651d74f7991490a20258961
GugaF <gugaf@networkgtc.com>
2023-04-26 20:51:02 +0200

    0223-04-26: 8th Kyu - U Can't Code Under Pressure

---
+++ cantCodeUnderPressure.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+// 8th Kyu - You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+function doubleInteger(i) {
+    return i * 2;
+}
+
+console.log(doubleInteger(2), 4);
+console.log(doubleInteger(4), 8);
+console.log(doubleInteger(-10), -20);
+console.log(doubleInteger(0), 0);
+console.log(doubleInteger(100), 200);
