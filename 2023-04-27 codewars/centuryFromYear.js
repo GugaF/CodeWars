9b8f303e8077dd31a117b5314de2d83887c37bbf
GugaF <gugaf@networkgtc.com>
2023-04-27 13:28:59 +0200

    2023-04-27: 8th Kyu - Century From Year

---
+++ 2023-04-27 codewars/centuryFromYear.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+// 8th Kyu - Century From Year
+
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+    return Math.ceil(year / 100);
+}
+
+console.log(century(1705), 18, 'Testing for year 1705');
+console.log(century(1900), 19, 'Testing for year 1900');
+console.log(century(1601), 17, 'Testing for year 1601');
+console.log(century(2000), 20, 'Testing for year 2000');
+console.log(century(89), 1, 'Testing for year 89');
