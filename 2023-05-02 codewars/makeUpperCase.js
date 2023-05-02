5a011530f5017551dd196b08c6c0e4d87c2052cb
GugaF <gugaf@networkgtc.com>
2023-05-02 21:22:13 +0200

    2023-05-02: 8th Kyu - Make Upper Case

---
+++ 2023-05-02 codewars/makeUpperCase.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+// 8th Kyu - Make Upper case
+
+// Write a function which converts the input string to uppercase.
+
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
+
+console.log(makeUpperCase(""),           "");
+console.log(makeUpperCase("hello"), "HELLO");
+console.log(makeUpperCase("Hello"), "HELLO");
+console.log(makeUpperCase("HELLO"), "HELLO");
