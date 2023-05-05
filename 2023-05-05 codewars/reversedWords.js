504f6808e6a296308ed8ef9f5df5f5ff5fa0d3ae
GugaF <gugaf@networkgtc.com>
2023-05-05 22:39:34 +0200

    2023-05-05: 8th Kyu - Reversed Words

---
+++ 2023-05-05 codewars/reversedWords.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e
+// 8th Kyu - Reversed Words
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+}
+
+console.log(reverseWords("hello world!"                 ),  "world! hello")
+console.log(reverseWords("yoda doesn't speak like this" ),  "this like speak doesn't yoda")
+console.log(reverseWords("foobar"                       ),  "foobar")
+console.log(reverseWords("kata editor"                  ),  "editor kata")
+console.log(reverseWords("row row row your boat"        ),  "boat your row row row")
+console.log(reverseWords(""                             ),  "")
