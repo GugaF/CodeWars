c215e1f77cacca04afacea6b65c01c8725c1b008
GugaF <gugaf@networkgtc.com>
2023-06-06 00:51:32 +0200

    2023-06-06: 8 Kyu - Crash Override

---
+++ 2023-06-06 codewars/crashOverride.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f
+// 8 Kyu - Crash Override
+
+// Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+// Notes:
+
+//     Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given. See the examples below for further details.
+
+//     If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+//     Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+
+var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache'}
+var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst'}
+
+function aliasGen(){
+    return// Code Here
+}
+
+console.log(aliasGen("Aike", "Cillington"), "Malware Mike");
+console.log(aliasGen("Fahima", "Tash"), "Function T-Rex");
+console.log(aliasGen("Daisy", "Petrovic"), "Data Payload");
+console.log(aliasGen("7393424", "Anumbha"), "Your name must start with a letter from A - Z.");
+console.log(aliasGen("Anuddanumbha", "23200"), "Your name must start with a letter from A - Z.");
