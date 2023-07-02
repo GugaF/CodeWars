d9867a6b8cb39e7e64042bc104e73caa7aff1693
GugaF <gugaf@networkgtc.com>
2023-07-02 22:56:30 +0200

    2023/07/02: 8 Kyu - Drink About

---
+++ 2023-07-02 codewars/drinkAbout.js
@@ -0,0 +1,36 @@
+// https://www.codewars.com/kata/56170e844da7c6f647000063
+// 8 Kyu - Drink About
+
+// Description:
+//     Kids drink toddy.
+//     Teens drink coke.
+//     Young adults drink beer.
+//     Adults drink whisky.
+
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+//     Children under 14 old.
+//     Teens under 18 old.
+//     Young under 21 old.
+//     Adults have 21 or more.
+
+// Examples: (Input --> Output)
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+
+function peopleWithAgeDrink(age) {
+    return age < 14 ? 'drink toddy' : 
+           age < 18 ? 'drink coke' :
+           age < 21 ? 'drink beer' : 'drink whisky';
+};
+
+console.log(peopleWithAgeDrink(13), 'drink toddy');
+console.log(peopleWithAgeDrink(17), 'drink coke');
+console.log(peopleWithAgeDrink(18), 'drink beer');
+console.log(peopleWithAgeDrink(20), 'drink beer');
+console.log(peopleWithAgeDrink(30), 'drink whisky');
