f75769f9558058497a99c714bea76853e63c56f4
GugaF <gugaf@networkgtc.com>
2023-07-04 22:57:03 +0200

    2023/07/03: 8 Kyu - How Good Are You Really?

---
+++ 2023-07-03 codewars/howGoodAreYouReally.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/5601409514fc93442500010b
+// 8 Kyu - How good are you really?
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+// Note:
+
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+    classPoints.push(yourPoints)  
+    const sum = classPoints.reduce(( a , b ) => a + b , 0)
+    return (sum / classPoints.length) <= yourPoints
+}
+
+console.log(betterThanAverage([2, 3], 5), true);
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true);
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9), false);
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50), false);
