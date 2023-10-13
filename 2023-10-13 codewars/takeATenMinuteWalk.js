9b8a8c4462ee5349d4e7af3f71043300cbf0a909
GugaF <gugaf@networkgtc.com>
2023-10-13 22:02:12 +0200

    2023/10/13: 6 Kyu - Take a Ten Minute Walk

--- 2023-10-13 codewars/takeATenMinuteWalk.js
+++ 2023-10-13 codewars/takeATenMinuteWalk.js
@@ -31,7 +31,7 @@ function isValidWalk(walk) {
     }
 }
 
-assert.isTrue(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'should return true');
-assert.isFalse(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'should return false');
-assert.isFalse(isValidWalk(['w']), 'should return false');
-assert.isFalse(isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'should return false');
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'should return true');
+console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'should return false');
+console.log(isValidWalk(['w']), 'should return false');
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'should return false');
