db1bed113559b11b152adfbde19584cdbd468a80
GugaF <gugaf@networkgtc.com>
2023-12-02 12:14:16 +0200

    2023/12/02: 8 Kyu - Broken Counter

---
+++ 2023-12-02 codewars/brokenCounter.js
@@ -0,0 +1,38 @@
+// https://www.codewars.com/kata/526471539d52735c620000c6
+// 8 Kyu - Broken Counter
+
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+
+function Counter() {
+    this.value = 0;
+}
+  
+Counter.prototype.increase() {
+    this.value++;
+};
+  
+Counter.prototype.getValue() {
+    return this.value;
+};
+  
+Counter.prototype.reset() {
+    this.value = 0;
+};
+
+
+// Anohter Way
+// class Counter() {
+//     constructor() { this.reset(); }
+//     increase()    { this.value++; }
+//     getValue()    { return this.value; }
+//     reset()       { this.value = 0; }
+// }
+
+
+var counter = new Counter();
+console.log(counter.getValue(), 0, 'Initial counter value must be 0');
+counter.increase();
+console.log(counter.getValue(), 1, 'Counter value must be increased');
+counter.reset();
+console.log(counter.getValue(), 0, 'Counter value must be 0 after reset');
