d42e67d632707ee6e99c6d5b6120655bb2c6b952
GugaF <gugaf@networkgtc.com>
2023-12-07 22:03:17 +0200

    2023/12/07: 7 Kyu - Fun with Classes #2 - Animals and Inheritance

--- 2023-12-03 codewars/dogsAndClasses.js
+++ 2023-12-03 codewars/dogsAndClasses.js
@@ -16,16 +16,42 @@ class Dog {
 }
 
 // You are then given a working class Labrador as your initial code.
-class Labrador {
+// class Labrador {
+//   constructor(name, age, gender, master) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//     this.species = "Labrador";
+//     this.legs = 4;
+//     this.size = "Large";
+//     this.master = master;
+//     this.loyal = true;
+//   }
+// }
+
+class Labrador extends Dog {
   constructor(name, age, gender, master) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.species = "Labrador";
-    this.legs = 4;
-    this.size = "Large";
-    this.master = master;
-    this.loyal = true;
+    super(name, age, gender, "Labrador", "Large", master, true);
   }
 }
 
+
+
+var spitsy = new Labrador("Spitsy", 10, "Male", "Donald");
+console.log(spitsy.name, "Spitsy");
+console.log(spitsy.age, 10);
+console.log(spitsy.gender, "Male");
+console.log(spitsy.species, "Labrador");
+console.log(spitsy.legs, 4);
+console.log(spitsy.size, "Large");
+console.log(spitsy.master, "Donald");
+console.log(spitsy.loyal, true);
+var edward = new Labrador("Edward", 3, "Male", "Emma");
+console.log(edward.name, "Edward");
+console.log(edward.age, 3);
+console.log(edward.gender, "Male");
+console.log(edward.species, "Labrador");
+console.log(edward.legs, 4);
+console.log(edward.size, "Large");
+console.log(edward.master, "Emma");
+console.log(edward.loyal, true);
