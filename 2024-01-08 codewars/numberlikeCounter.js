58c76771cefd2a4c7bed9155ddef1ddeaa889268
GugaF <gugaf@networkgtc.com>
2024-01-08 07:11:38 +0200

    2024/01/08: 7 Kyu - Number-Like Counter

---
+++ 2024-01-08 codewars/numberlikeCounter.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/5313b713bb244a0eb20001fe
+// 7 Kyu - Number-like counter
+
+// You're going to implement a simple counter class. The counter will start at zero, 
+// and every time its incr method is called, it will increase by 1.
+
+// There's one caveat: Your counter must act like a number and support arithmetic 
+// operations and comparisons.
+
+// For example:
+// var c = new Counter();
+// c.incr(); // counter is now at 1
+// c + 1; // 2
+// c > 1; // false
+// c > 0; // true
+// c == 1; // true
+// Math.sqrt(c); // 1
+
+// You are not required to support equality comparison between two Counter instances.
+
+function Counter(){
+    this.value = 0;
+    this.incr = function() { this.value++; };
+    this.valueOf = function() { return this.value; };
+}
+
+var c = new Counter();
+c.incr(); // counter is now at 1
+console.log(c + 1, 2);
+console.log(c > 1, false);
+console.log(c > 0, true);
+console.log(c == 1, true);
+console.log(Math.sqrt(c), 1);
