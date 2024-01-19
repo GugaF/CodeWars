12adbe6d16a54e9e9a91857af939316dae183d4f
GugaF <gugaf@networkgtc.com>
2024-01-19 23:40:33 +0200

    2024/01/19: 7 Kyu - Static Privacy

---
+++ 2024-01-19 codewars/staticPrivacy.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/652a19cd7e92f356f437d059
+// 7 Kyu - Static Privacy
+
+// I wrote a class Class, with a private static property 
+// foo and a getter to retrieve it:
+
+// class Class {
+//   static #foo = 42;
+//   static get foo() {
+//     return this.Class;
+//   }
+// }
+// Class.foo; // 42
+
+// However, when I create a class Subclass that inherits from Class, the code breaks:
+
+// class Subclass extends Class {}
+// Subclass.foo; // Error !
+
+// Can you fix the code ?
+// Modify Class so that Subclass.foo will work correctly. This should also work 
+// for any level of inheritance from Class, e.g. with:
+
+// class Deepclass extends Subclass {}
+// Deepclass.foo; // should return 42
+
+class Class {
+    static foo = 42;
+  
+    static get foo() {
+      return Class.foo;
+    }
+  }
+  
+  class Subclass extends Class{}
+  class Deepclass extends Subclass{}
+  
+  console.log(Class.foo, 42)
+  console.log(Subclass.foo, 42)
+  console.log(Deepclass.foo, 42)
